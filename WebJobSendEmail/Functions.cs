db12aae1d249a5ab05cafe0fb6da51edd42adf21
Richie <richie@codemoggy.com>
2016-05-11 23:09:06 -0400

    Initial Commit

---
+++ WebJobSendEmail/Functions.cs
@@ -0,0 +1,60 @@
+﻿//The MIT License (MIT)
+
+//Copyright (c) Microsoft Corporation
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using SendGrid;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace WebJobSendEmail
+{
+    public class Functions
+    {
+        // Runs once every 30 seconds
+        public static void TimerJob([TimerTrigger("00:00:30")] TimerInfo timer,
+            [SendGrid(Subject = "Important News", Text = "The timer has fired and this is your notification")] SendGridMessage message)
+        {
+            try
+            {
+                List<MailAddress> recipients = new List<MailAddress>() { new MailAddress(ConfigurationManager.AppSettings.Get("recipient")) };
+
+                message.To = recipients.ToArray();
+
+                var webTransport = new SendGrid.Web(ConfigurationManager.AppSettings.Get("AzureWebJobsSendGridApiKey"));
+
+                webTransport.DeliverAsync(message).Wait();
+
+                Console.WriteLine("Success: mail sent to recipient!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ WebJobSendEmail/Program.cs
@@ -0,0 +1,59 @@
+﻿//The MIT License (MIT)
+
+//Copyright (c) Microsoft Corporation
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.SendGrid;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace WebJobSendEmail
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        static void Main()
+        {
+            var sender = ConfigurationManager.AppSettings.Get("sender");
+            var senderName = ConfigurationManager.AppSettings.Get("senderName");
+
+            var config = new JobHostConfiguration();
+
+            config.UseSendGrid(new SendGridConfiguration()
+            {
+                FromAddress = new MailAddress(sender, senderName)
+            });
+
+            config.UseTimers();
+
+            var host = new JobHost(config);
+
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
